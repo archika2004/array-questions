d6309be36928de92ccd7c9956eba01a5e93077f5
archika2004 <archikaalok2004@gmail.com>
2026-03-23 22:11:33 +0530

    Add files via upload

---
+++ MergeSortedArrays.java
@@ -0,0 +1,28 @@
+public class MergeSortedArrays{
+    public static void main(String[] args) {
+        int nums1[]={1,2,3,0,0,0};int n=3;
+        int nums2[]={2,5,6};int m=3;
+        int i=n-1;
+        int j=m-1;
+        int k=m+n-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+        for(int l=0;l<nums1.length;l++){
+            System.out.println(nums1[l]+" ");
+        }
+    }
+}
