2fed56111909518cac266ea6d25ecbd85900d0b1
archika2004 <archikaalok2004@gmail.com>
2026-03-23 21:04:46 +0530

    Add files via upload

---
+++ BuyandSellStocks.java
@@ -0,0 +1,19 @@
+public class BuyandSellStocks {
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }
+            else{
+                int profit=prices[i]-minPrice;
+                if(profit>maxProfit){
+                    maxProfit=profit;
+                }
+            }
+        }
+        System.out.println(maxProfit);
+    }
+}
